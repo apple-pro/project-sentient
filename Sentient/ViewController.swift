dd5fefdc5505a903d198e46e4bfef678ff5054cb
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-15 19:51:24 +0800

    Add predictor code

--- Sentient/ViewController.swift
+++ Sentient/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var textInput: UITextField!
+    
+    let classifier = TweetSentimentClassifier()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predict(_ sender: UIButton) {
+        if let text = textInput.text {
+            let prediction = try! classifier.prediction(text: text)
+            let sentiment = SentimentResult(rawValue: prediction.label)
+            resultLabel.text = sentiment?.face
+        }
+    }
+    
 }
 
+enum SentimentResult: String {
+    case pos = "Pos", neutral = "Neutral", neg = "Neg"
+}
+
+extension SentimentResult {
+    
+    var face: String {
+        switch self {
+        case .neg:
+            return "🤬"
+        case .pos:
+            return "😻"
+        case .neutral:
+            return "😑"
+        }
+    }
+}
