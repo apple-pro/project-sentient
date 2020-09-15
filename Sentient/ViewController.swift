d6b0580006a26df6ddf822c9153f05daa82b2550
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-15 18:50:52 +0800

    Initial Commit

---
+++ Sentient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sentient
+//
+//  Created by StartupBuilder.INFO on 9/15/20.
+//  Copyright © 2020 StartupBuilder.INFO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
